438d251e3ff3299d0bd1d438737664ccbf3772b2
reaue <mathias.crama.second@gmail.com>
2026-09-07 14:52:34 +0200

    adding searching and a random plant button

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+const API_KEY = import.meta.env.VITE_TREFLE_API_KEY
+
+
 let darkmode = localStorage.getItem('darkmode') === "active";
 const themeSwitch = document.getElementById("theme-switch");
 
@@ -18,4 +21,52 @@ if (darkmode) enableDarkmode()
 
 themeSwitch.addEventListener("click", () =>{
     darkmode ? disableDarkmode() : enableDarkmode();
+});
+
+
+let plantsWithImage = []
+
+
+fetch(`https://trefle.io/api/v1/plants?token=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {plantsWithImage = data.data.filter(plant => plant.image_url);
+        get_show_plant();
+    });
+
+
+function get_show_plant (name=false) {
+    if (!name) {
+        name = plantsWithImage[Math.floor(Math.random() * plantsWithImage.length)].common_name; 
+    };
+
+    fetch(`https://trefle.io/api/v1/species/search?token=${API_KEY}&q=${encodeURIComponent(name)}`)
+        .then(response => response.json())
+        .then(data => {
+            if (!data.data || data.data.length === 0) {
+                document.querySelector(".common-name").innerHTML = "Plant not found";
+                document.querySelector(".scientific-name").innerHTML = "Try another research";
+
+                return;
+            };
+
+            const plant = data.data[0];
+
+            document.querySelector(".common-name").innerHTML = plant.common_name
+            document.querySelector(".scientific-name").innerHTML = plant.scientific_name
+            document.querySelector(".img-show").src = plant.image_url;
+        });
+};
+
+document.getElementById("btn-random").addEventListener("click", () => {
+    get_show_plant();
+});
+
+document.getElementById("search-form").addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+    const name = document.getElementById("tsearch").value.trim();
+    console.log("SEARCH:", name);
+    if (name !== "") {
+        get_show_plant(name);
+    };
 });
